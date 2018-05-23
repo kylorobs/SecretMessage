75a5aa2596f7740f20875ca69ece8662eb4b1910
Kyle <Kyle@idea-PC.com>
2018-05-23 09:01:34 +0100

    Each Yes button selected

--- Resources/js/index.js
+++ Resources/js/index.js
@@ -1,16 +1,31 @@
 $(document).ready(function(){
 
-  // style Yes and No buttons
+  // Actions for Yes and No buttons
   $('.yes').on('click', function(){
-   $(this).addClass('green')
-          .attr('val','correct');
-     $('.no').removeClass('red')
-             .attr('val', '');
+    const $q1 = $(this).parent()
+                       .attr('val');
+
+                       $(this).addClass('green')
+                              .attr('val','correct');
+                       $('.no').removeClass('red')
+                              .attr('val', '');
+
+    if ($q1 === 'Q1'){
+      $(this).hide();
+    }
+    else if ($q1 === 'Q2'){
+      $(this).hide();
+    }
+    else if ($q1 === 'Q3'){
+      $(this).hide();
+    }
 
   });
 
+
+
     $('.no').on('click', function(){
-   $(this).addClass('red');
+   $(this).addClass('red')
           .attr('val','wrong');
     $('.yes').removeClass('green')
              .attr('val', '');
@@ -20,24 +35,31 @@ $(document).ready(function(){
 
   // Help Errors
   $('.submit').on('click', function(){
-let $wrongQ1 = $('.yes').attr('val');
-let $correctQ1 = $('#Q1 .answer .yes')..attr('val')
-let $correctQ2 =
-let $correctQ3 =
+const $correctQ1 = $('#Q1 .answer .yes').attr('val');
+const $correctQ2 = $('#Q2 .answer .no').attr('val');
+const $correctQ3 =$('#Q3 .answer .yes').attr('val');
 
-let $wrongQ1 =
-let $wrongQ2 =
-let $wrongQ3 =
+const $wrongQ1 = $('#Q1 .answer .no').attr('val');
+const $wrongQ2 =$('#Q2 .answer .yes').attr('val');
+const $wrongQ3 =$('#Q3 .answer .no').attr('val');
+
+  if ($correctQ1 === 'correct'|| $correctQ2 === 'correct' || $correctQ3 === 'correct') {
+     $('.slide').hide();
+  }
+  else if ($wrongQ1 === 'wrong') {
+    $('#Q1 .help p').text('Of course we have hyenahs! Lions too.');
+  }
+  else if ($wrongQ2 === 'wrong') {
+    $('#Q1 .help p').text('I am lost for words');
+  }
+  else if ($wrongQ3 === 'wrong') {
+    $('#Q1 .help p').text('Come now! You should know this.');
+  }
+
 
  });
 
 
- $('.submit').on('click', function(){
-   let $correct = $('.yes').attr('val');
-   if ($correct === 'answer'){
-     $('.help p').text('What are you thinking?');
-   }
- });
 
 
 
